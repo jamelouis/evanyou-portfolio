5d84ed075935bce8364dbfe4086480922d87a6a8
jamelouis <jamelouis2015@gmail.com>
2025-03-26 17:43:50 +0800

    config base property for serve path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vite-vue-bun-template/',
+  base: '/evanyou-portfolio/',
   plugins: [vue()],
 })
