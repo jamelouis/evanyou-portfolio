ecaf1928ef53c2343f20ecd1f11bed6961937c1e
jamelouis <jamelouis2015@gmail.com>
2025-03-30 22:35:58 +0800

    refactor Footer Component with watch

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,28 @@
 <script setup>
+import { ref, watch, computed } from 'vue';
+const count = ref(0);
+const quote = ref("点击更新随机一言");
+
+watch(count, async (newCount) => {
+    try {
+      const res = await fetch("https://uapis.cn/api/say")
+      quote.value = await (res.text());
+    } catch (error) {
+    } finally {
+    }
+})
+
+const msg = computed(()=>{
+    return count.value + ": " + quote.value;
+})
+
 </script>
 
 <template>
     <footer>
+        <p class="clickable" @click="count++">
+            {{ msg  }}
+        </p>
         <p>
             &copy;2025
             <a href="https://evanyou.me/">evanyou</a>. All rights reserved.
@@ -17,5 +37,9 @@ footer {
     background-color: #2c3e50;
     color: #ecf0f1;
     margin-top: 3rem;
+}
+
+.clickable {
+    cursor: pointer;
 }
 </style>
